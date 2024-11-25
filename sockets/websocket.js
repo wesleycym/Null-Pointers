cf6be49241250be02c76bc1b7aecc368977d736a
wesleycym <wesleycy@buffalo.edu>
2024-11-25 09:29:49 -0500

    Starter websocket file

---
+++ sockets/websocket.js
@@ -0,0 +1,10 @@
+import { WebSocketServer } from 'ws'; // import WebSocketServer from ws
+
+// Websocket events: (Direct Message)
+//  send_message | client -> server
+//  receive_message | server -> client
+
+function setupWebSocket(server, db)
+{
+    return;
+}
